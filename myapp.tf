81011652cedaa268ff16a42e20a4da4d601dd672
Patryk Futa <patryk.futa@gmail.com>
2021-12-29 18:02:27 +0100

    update

--- myapp.tf
+++ myapp.tf
@@ -8,7 +8,7 @@ data "template_file" "myapp-task-definition-template" {
   template = file("templates/app.json.tpl")
   vars = {
     REPOSITORY_URL = replace(data.aws_ecr_repository.myapp.repository_url, "https://", "")
-    APP_VERSION = "latest"
+    APP_VERSION    = var.MYAPP_VERSION
   }
 }
 
@@ -17,6 +17,27 @@ resource "aws_ecs_task_definition" "myapp-task-definition" {
   container_definitions = data.template_file.myapp-task-definition-template.rendered
 }
 
+resource "aws_ecs_service" "myapp-service" {
+  count           = var.MYAPP_SERVICE_ENABLE
+  name            = "myapp"
+  cluster         = aws_ecs_cluster.example-cluster.id
+  task_definition = aws_ecs_task_definition.myapp-task-definition.arn
+  desired_count   = 3
+  iam_role        = aws_iam_role.ecs-service-role.arn
+  depends_on      = [aws_iam_policy_attachment.ecs-service-attach1]
+
+  load_balancer {
+    elb_name       = aws_elb.myapp-elb.name
+    container_name = "myapp"
+    container_port = 3000
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+}
+
+# ELB
 resource "aws_elb" "myapp-elb" {
   name = "myapp-elb"
 
@@ -45,24 +66,6 @@ resource "aws_elb" "myapp-elb" {
 
   tags = {
     Name = "myapp-elb"
+    ProjectName = var.project_name
   }
 }
-
-resource "aws_ecs_service" "myapp-service" {
-  name            = "myapp"
-  cluster         = aws_ecs_cluster.example-cluster.id
-  task_definition = aws_ecs_task_definition.myapp-task-definition.arn
-  desired_count   = 1
-  iam_role        = aws_iam_role.ecs-service-role.arn
-  depends_on      = [aws_iam_policy_attachment.ecs-service-attach1]
-
-  load_balancer {
-    elb_name       = aws_elb.myapp-elb.name
-    container_name = "myapp"
-    container_port = 3000
-  }
-  lifecycle {
-    ignore_changes = [task_definition]
-  }
-}
-
